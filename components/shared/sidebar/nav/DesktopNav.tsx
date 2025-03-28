08768e69b2a5d064b5ae78e8e60a3769a44d6a74
Aashay30 <tamrakaraashay30@gmail.com>
2025-03-28 16:03:04 +0530

    add ItemsList component for conversation display

--- components/shared/sidebar/nav/DesktopNav.tsx
+++ components/shared/sidebar/nav/DesktopNav.tsx
@@ -1,18 +1,18 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import { ThemeToggle } from '@/components/ui/theme/theme-toggle';
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { ThemeToggle } from "@/components/ui/theme/theme-toggle";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { UserButton } from '@clerk/nextjs';
-import Link from 'next/link';
-import { useNavigation } from '../../../../app/hooks/useNavigation';
-import { Badge } from '../../../ui/badge';
+} from "@/components/ui/tooltip";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { useNavigation } from "../../../../app/hooks/useNavigation";
+import { Badge } from "../../../ui/badge";
 const DesktopNav = () => {
-  const paths = useNavigation();
+const paths = useNavigation();
 
   return (
     <Card className="hidden lg:flex lg:flex-col lg:justify-between lg:items-center lg:h-full lg:w-16 lg:px-2 lg:py-4">
@@ -26,7 +26,7 @@ const DesktopNav = () => {
                     <TooltipTrigger>
                       <Button
                         size="icon"
-                        variant={path.active ? 'default' : 'outline'}
+                        variant={path.active ? "default" : "outline"}
                       >
                         {path.icon}
                       </Button>
